cd7805cf7914cbab53c4d1c0a15c3ec59050a1f0
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-15 06:53:32 -0800

    changes

--- main.c
+++ main.c
@@ -10,56 +10,58 @@
 int main(int argc, char *argv[])
 {
 	FILE *file;
-    	char line[100];  /* Adjust the buffer size as needed */
-    	unsigned int line_number = 0;
-    	stack_t *stack = NULL;
-    	stack_t *current;
+	char line[100];  /* Adjust the buffer size as needed */
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+	stack_t *current;
 
-    	if (argc != 2)
-    	{
-        	fprintf(stderr, "USAGE: monty file\n");
-        	return EXIT_FAILURE;
-    	}
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-    	file = fopen(argv[1], "r");
-    	if (file == NULL)
-    	{
-        	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        	return EXIT_FAILURE;
-    	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
-    	while (fgets(line, sizeof(line), file) != NULL)
-    	{
-        	char *opcode;
-        	int value;
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		char *opcode;
+		int value;
 
-        	line_number++;
+		line_number++;
 
 		/* Skip empty lines or comments */
 		if (strlen(line) == 0 || line[0] == '#' || line[0] == '\n')
 			continue;
 
-        	opcode = strtok(line, " \t\n");
-        	if (opcode == NULL)
-            	continue;
+		opcode = strtok(line, " \t\n");
+		if (opcode == NULL)
+		continue;
 
-        	if (strcmp(opcode, "push") == 0)
-        	{
-            		char *arg = strtok(NULL, " \t\n");
-            		if (arg == NULL)
-            		{
-                		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                		fclose(file);
-                		return EXIT_FAILURE;
-            		}
+		if (strcmp(opcode, "push") == 0)
+		{
+			char *arg = strtok(NULL, " \t\n");
 
-            		value = atoi(arg);
-            		push(&stack, value);
-        	}
-        	else if (strcmp(opcode, "pall") == 0)
-        	{
-            	pall(&stack);
-        	}
+			if (arg == NULL)
+
+			{
+				fprintf(stderr, "L%u: usage: push integer\n", line_number);
+				fclose(file);
+				return (EXIT_FAILURE);
+			}
+
+			value = atoi(arg);
+			push(&stack, value);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+		pall(&stack);
+		}
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
@@ -72,6 +74,7 @@ int main(int argc, char *argv[])
 	while (current != NULL)
 	{
 		stack_t *temp = current;
+
 		current = current->next;
 		free(temp);
 	}
